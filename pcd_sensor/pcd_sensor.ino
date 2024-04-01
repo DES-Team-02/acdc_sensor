6e47c76490b4ad6580b64da753f4d16b8317587e
SeungWoo-L <seungwoo991123@gmail.com>
2024-04-01 16:19:06 +0200

    modify structure

--- pcd_sensor/pcd_sensor.ino
+++ pcd_sensor/pcd_sensor.ino
@@ -2,66 +2,58 @@
 #include <SPI.h>
 #include <mcp2515_can.h>
 
-#define BAUD_RATE 115200
-#define SONAR_NUM 3 // Number of sensors.
-#define MAX_DISTANCE 400 // Maximum distance (in cm) to ping.
+#define BAUD_RATE 115200 // Serial baud rate
+#define SONAR_NUM 3 // Number of ultrasonic sensors.
+#define MAX_DISTANCE 400 // Maximum distance (in cm) that can be measured.
 #define CAN_SAMPLE_RATE CAN_500KBPS // CAN bus speed
-#define CAN_HAT_CS_PIN 9
+#define CAN_HAT_CS_PIN 9 // Pin number for the CAN controller chip select
 
-// Sensor pins
-#define SENSOR_FRONT_LEFT_TRIG_PIN 5
-#define SENSOR_FRONT_LEFT_ECHO_PIN 6
-#define SENSOR_FRONT_MIDDLE_TRIG_PIN 7
-#define SENSOR_FRONT_MIDDLE_ECHO_PIN 8
-#define SENSOR_FRONT_RIGHT_TRIG_PIN 9
-#define SENSOR_FRONT_RIGHT_ECHO_PIN 10
+// Pins for the ultrasonic sensors
+#define SENSOR_FRONT_LEFT_TRIG_PIN 3
+#define SENSOR_FRONT_LEFT_ECHO_PIN 4
+#define SENSOR_FRONT_MIDDLE_TRIG_PIN 5
+#define SENSOR_FRONT_MIDDLE_ECHO_PIN 6
+#define SENSOR_FRONT_RIGHT_TRIG_PIN 7
+#define SENSOR_FRONT_RIGHT_ECHO_PIN 8
 
-unsigned short cm[SONAR_NUM]; // Where the ping distances are stored.
+unsigned short cm[SONAR_NUM]; // Array to store distances measured by sensors.
+uint8_t data_buf[SONAR_NUM * 2]; // Data buffer for CAN message
+const unsigned long can_id = 0x200; // CAN device address
+mcp2515_can CAN(CAN_HAT_CS_PIN); // CAN bus object
 
-// Sensor object array
+// Ultrasonic sensor objects
 NewPing sonar[SONAR_NUM] = {
   NewPing(SENSOR_FRONT_LEFT_TRIG_PIN, SENSOR_FRONT_LEFT_ECHO_PIN, MAX_DISTANCE),
   NewPing(SENSOR_FRONT_MIDDLE_TRIG_PIN, SENSOR_FRONT_MIDDLE_ECHO_PIN, MAX_DISTANCE),
   NewPing(SENSOR_FRONT_RIGHT_TRIG_PIN, SENSOR_FRONT_RIGHT_ECHO_PIN, MAX_DISTANCE)
 };
 
-const unsigned long can_id = 0x200; // CAN Device address
-mcp2515_can CAN(CAN_HAT_CS_PIN); // CAN Bus object
-
 void setup() {
   Serial.begin(BAUD_RATE);
-  while (!Serial) {}; // Wait for serial port to connect.
+  while (!Serial) {}; // Wait for the serial port to connect. Necessary for native USB port only.
   if (CAN_OK != CAN.begin(CAN_SAMPLE_RATE)) {
-    Serial.println("CAN BUS Shield init fail");
+    Serial.println("CAN BUS Shield init fail"); // Initialization failed
     delay(100);
   } else {
-    Serial.println("CAN BUS Shield init ok!");
+    Serial.println("CAN BUS Shield init ok!"); // Initialization successful
   }
 }
 
 void loop() {
   triggerSensors();
   delay(100); // Delay to allow for sensor echo returns
-  readSensors();
-  sendCANMessage();
+  oneSensorCycle();
   delay(50); // Delay between readings
 }
 
 void triggerSensors() {
   for (uint8_t i = 0; i < SONAR_NUM; i++) {
-    sonar[i].ping(); // Trigger each sensor to ping
+    sonar[i].ping(); // Trigger each sensor to start measurement
   }
 }
 
-void readSensors() {
-  for (uint8_t i = 0; i < SONAR_NUM; i++) {
-    cm[i] = sonar[i].ping_cm(); // Read each sensor
-  }
-}
-
-void sendCANMessage() {
-  uint8_t len_frame = SONAR_NUM * sizeof(unsigned short);
-  uint8_t data_buf[len_frame];
+void oneSensorCycle() { 
+  readSensors(); // Read measurements from each sensor
 
   // Load data_buf with sensor data
   for (int i = 0, j = 0; i < SONAR_NUM; i++, j += 2) {
@@ -69,6 +61,22 @@ void sendCANMessage() {
     data_buf[j + 1] = lowByte(cm[i]);
   }
 
+  // Print sensor data to the serial monitor in hexadecimal format
+  Serial.println("-----------------------------");
+  Serial.print("Set data from ID: 0x");
+  Serial.println(can_id, HEX); // Print CAN ID
+  for (int i = 0; i < sizeof(data_buf); i++) { 
+    Serial.print(data_buf[i], HEX); // Print data bytes
+    Serial.print("\t");
+  }
+  Serial.println();
+
   // Send CAN message
-  CAN.sendMsgBuf(can_id, 0, len_frame, data_buf);
+  CAN.sendMsgBuf(can_id, 0, sizeof(data_buf), data_buf);                   
+}
+
+void readSensors() {
+  for (uint8_t i = 0; i < SONAR_NUM; i++) {
+    cm[i] = sonar[i].ping_cm(); // Read distance from each sensor and store it in the cm array
+  }
 }
