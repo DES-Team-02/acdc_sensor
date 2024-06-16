918c565c3e91e4eef1b9f090875f40bd0d3c7db7
jinholee <jinholee@student.42seoul.kr>
2024-06-16 20:34:41 +0900

    using front, rear sensor only

--- pcd_sensor/pcd_sensor.ino
+++ pcd_sensor/pcd_sensor.ino
@@ -3,19 +3,21 @@
 #include <mcp2515_can.h>
 
 #define BAUD_RATE 115200 // Serial baud rate
-#define SONAR_NUM 3 // Number of ultrasonic sensors.
+#define SONAR_NUM 2 // Number of ultrasonic sensors.
 #define MEASURE_ITERATION 3 // Number of measuring iterations for each sensor.
 #define MAX_DISTANCE 200 // Maximum distance (in cm) that can be measured.
 #define CAN_SAMPLE_RATE CAN_500KBPS // CAN bus speed
 #define CAN_HAT_CS_PIN 9 // Pin number for the CAN controller chip select
 
 // Pins for the ultrasonic sensors
-#define SENSOR_FRONT_LEFT_TRIG_PIN 3
-#define SENSOR_FRONT_LEFT_ECHO_PIN 4
-#define SENSOR_FRONT_MIDDLE_TRIG_PIN 5
-#define SENSOR_FRONT_MIDDLE_ECHO_PIN 6
-#define SENSOR_FRONT_RIGHT_TRIG_PIN 7
-#define SENSOR_FRONT_RIGHT_ECHO_PIN 8
+#define SENSOR_FRONT_VCC_PIN  1
+#define SENSOR_FRONT_TRIG_PIN 2
+#define SENSOR_FRONT_ECHO_PIN 3
+#define SENSOR_FRONT_GND_PIN  4
+#define SENSOR_REAR_VCC_PIN   5
+#define SENSOR_REAR_TRIG_PIN  6
+#define SENSOR_REAR_ECHO_PIN  7
+#define SENSOR_REAR_GND_PIN   8
 
 unsigned short cm[SONAR_NUM]; // Array to store distances measured by sensors.
 uint8_t data_buf[SONAR_NUM * 2]; // Data buffer for CAN message
@@ -26,12 +28,20 @@ float factor = sqrt(1 + temp / 273.15) / 60.368; // Factor to convert millisecon
 
 // Ultrasonic sensor objects
 NewPing sonar[SONAR_NUM] = {
-  NewPing(SENSOR_FRONT_LEFT_TRIG_PIN, SENSOR_FRONT_LEFT_ECHO_PIN, MAX_DISTANCE),
-  NewPing(SENSOR_FRONT_MIDDLE_TRIG_PIN, SENSOR_FRONT_MIDDLE_ECHO_PIN, MAX_DISTANCE),
-  NewPing(SENSOR_FRONT_RIGHT_TRIG_PIN, SENSOR_FRONT_RIGHT_ECHO_PIN, MAX_DISTANCE)
+  NewPing(SENSOR_FRONT_TRIG_PIN, SENSOR_FRONT_ECHO_PIN, MAX_DISTANCE),
+  NewPing(SENSOR_REAR_TRIG_PIN, SENSOR_REAR_ECHO_PIN, MAX_DISTANCE)
 };
 
 void setup() {
+  pinMode(SENSOR_FRONT_VCC_PIN, OUTPUT);
+  pinMode(SENSOR_FRONT_GND_PIN, OUTPUT);
+  pinMode(SENSOR_REAR_VCC_PIN, OUTPUT);
+  pinMode(SENSOR_REAR_GND_PIN, OUTPUT);
+  digitalWrite(SENSOR_FRONT_VCC_PIN, HIGH);
+  digitalWrite(SENSOR_FRONT_GND_PIN, LOW);
+  digitalWrite(SENSOR_REAR_VCC_PIN, HIGH);
+  digitalWrite(SENSOR_REAR_GND_PIN, LOW);
+
   Serial.begin(BAUD_RATE);
   while (!Serial) {}; // Wait for the serial port to connect. Necessary for native USB port only.
   if (CAN_OK != CAN.begin(CAN_SAMPLE_RATE)) {
@@ -44,7 +54,7 @@ void setup() {
 
 void loop() {
   oneSensorCycle();
-  delay(100);
+  delay(50);
 }
 
 void oneSensorCycle() { 
@@ -72,6 +82,6 @@ void oneSensorCycle() {
 
 void readSensors() {
   for (uint8_t i = 0; i < SONAR_NUM; i++) {
-    cm[i] = (short)(sonar[i].ping_median(MEASURE_ITERATION, MAX_DISTANCE) * factor); // Read distance from each sensor and store it in the cm array
+    cm[i] = (short)(sonar[i].ping_median(MEASURE_ITERATION, MAX_DISTANCE) * factor, 50); // Read distance from each sensor and store it in the cm array
   }
 }
